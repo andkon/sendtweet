487e6a960b35e7472cf6256ebde02a69ba96ee40
Andrew Konoff <andrew.konoff@gmail.com>
2019-07-02 19:53:18 -0400

    Initial Commit

---
+++ sendtweet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sendtweet
+//
+//  Created by Andrew Konoff on 2019-07-02.
+//  Copyright © 2019 Strings.fm Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
